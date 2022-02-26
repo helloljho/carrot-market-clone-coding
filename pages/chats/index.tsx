8589ba104983b71e174b6376b822bb9e38c97454
helloljho <helloljho@gmail.com>
2022-02-26 12:58:03 +0900

    fix : set all pages layout

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,26 @@
 import type { NextPage } from 'next';
+import Layout from '../../components/layout';
 
 const Chats: NextPage = () => {
   return (
-    <div className="py-10  divide-y-[1px]">
-      {[1, 2, 3, 4, 5].map((item, index) => (
-        <div
-          key={index}
-          className="flex mb-3 px-4 items-center space-x-3 py-3 cursor-pointer"
-        >
-          <div className="w-10 h-10 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">Steve Jebs</p>
-            <p className="text-sm text-gray-500 block">
-              See you tomorrow in the coner at 2pm
-            </p>
+    <Layout title="채팅" hasTabBar>
+      <div className="py-10  divide-y-[1px]">
+        {[1, 2, 3, 4, 5].map((item, index) => (
+          <div
+            key={index}
+            className="flex mb-3 px-4 items-center space-x-3 py-3 cursor-pointer"
+          >
+            <div className="w-10 h-10 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">Steve Jebs</p>
+              <p className="text-sm text-gray-500 block">
+                See you tomorrow in the coner at 2pm
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
