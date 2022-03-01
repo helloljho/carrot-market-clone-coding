582b0137713b0c9642c642b9b02df60a9ec441ed
helloljho <helloljho@gmail.com>
2022-03-01 13:36:48 +0900

    fix : enter api

---
+++ pages/form.tsx
@@ -0,0 +1,56 @@
+import { FieldErrors, useForm } from 'react-hook-form';
+
+interface FormProps {
+  username: string;
+  email: string;
+  password: string;
+}
+export default function Forms() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormProps>();
+
+  const onValid = (data: FormProps) => {
+    console.log(data);
+  };
+
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register('username', {
+          required: 'Username is required',
+          minLength: {
+            message: 'The user should  be loinger than 5 chars',
+            value: 5,
+          },
+        })}
+        type="text"
+        placeholder="username"
+      />
+      <input
+        {...register('email', {
+          required: 'Email is required',
+          validate: {
+            notGmail: (value) =>
+              !value.includes('@gmail.com') ? '' : 'Gmail is now allowed',
+          },
+        })}
+        type="email"
+        placeholder="email"
+      />
+      {errors.email?.message}
+      <input
+        {...register('password', { required: 'Password is required' })}
+        type="password"
+        placeholder="password"
+      />
+      <input type="submit" value="create account" />
+    </form>
+  );
+}
