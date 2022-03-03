f3f0d5210ba1b16dd744ac48726b84664c78bf4f
helloljho <helloljho@gmail.com>
2022-03-03 17:31:52 +0900

    fix : Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,29 +1,48 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import withHandler from '@libs/server/withHandler';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
+import twilio from 'twilio';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   console.log(req.body);
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) {
+    return res.status(400).json({ ok: false });
+  }
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
   const token = await client.token.create({
     data: {
-      payload: '1234',
+      payload,
       user: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: 'Anomymous',
-            ...payload,
+            ...user,
           },
         },
       },
     },
   });
-  console.log(token);
-  return res.status(200).end();
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_SERVICE_SID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
+  return res.status(200).json({
+    ok: true,
+  });
 }
 
 export default withHandler('POST', handler);
