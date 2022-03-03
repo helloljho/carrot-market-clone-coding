aa91f049e39c939b8fc4fc1dd2c8cb26f147c054
helloljho <helloljho@gmail.com>
2022-03-03 16:43:27 +0900

    fix : add token model

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,50 +6,23 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: '1234',
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: 'Anomymous',
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: 'Anomymous',
-      ...payload,
-    },
-    update: {},
   });
-  // if (email) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email,
-  //     },
-  //   });
-  //   if (!user) {
-  //     console.log('Did not found. Will create');
-  //     user = await client.user.create({
-  //       data: {
-  //         name: 'Anomymous',
-  //         email,
-  //       },
-  //     });
-  //   }
-  // }
-  // if (phone) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone: parseInt(phone),
-  //     },
-  //   });
-  //   if (!user) {
-  //     console.log('Did not found. Will create');
-  //     user = await client.user.create({
-  //       data: {
-  //         name: 'Anomymous',
-  //         phone: parseInt(phone),
-  //       },
-  //     });
-  //   }
-  // }
-
-  console.log(user);
+  console.log(token);
   return res.status(200).end();
 }
 
