526cae7a5c0f1ee89e06f4bfc280a64b60e7d804
helloljho <helloljho@gmail.com>
2022-03-07 10:42:09 +0900

    fix : Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -12,12 +12,12 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  console.log(req.body);
   const { phone, email } = req.body;
   const user = phone ? { phone } : email ? { email } : null;
   if (!user) {
     return res.status(400).json({ ok: false });
   }
+
   const payload = Math.floor(100000 + Math.random() * 900000) + '';
   const token = await client.token.create({
     data: {
