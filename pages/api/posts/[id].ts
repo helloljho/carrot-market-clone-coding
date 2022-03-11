5b376224f72e28b721ebd8fc7af48035b19e0ea4
helloljho <helloljho@gmail.com>
2022-03-11 12:09:07 +0900

    fix : Community detail

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+import { withApiSession } from '@libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { id } = req.query;
+
+  const post = await client.post.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      answers: {
+        select: {
+          id: true,
+          answer: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wondering: true,
+        },
+      },
+    },
+  });
+  console.log(post);
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }));
