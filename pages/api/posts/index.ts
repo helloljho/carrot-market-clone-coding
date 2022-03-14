b986ed9f17c3c0c9f56c45fdd042812869474ab8
helloljho <helloljho@gmail.com>
2022-03-14 12:27:24 +0900

    fix : useCoords

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -8,13 +8,16 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const {
-    body: { question },
+    body: { question, latitude, longitude },
     session: { user },
   } = req;
+  console.log(latitude, longitude);
   if (req.method === 'POST') {
     const post = await client.post.create({
       data: {
         question,
+        latitude,
+        longitude,
         user: {
           connect: {
             id: user?.id,
